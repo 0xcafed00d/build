cf998908fb466b495d8ba0dfcf03ba2a497eb6bc
Lee Witek <neomech@gmail.com>
2015-09-13 20:04:06 +0100

    updated build_vim

--- build_vim.sh
+++ build_vim.sh
@@ -11,6 +11,8 @@ sudo apt-get install lua5.1 liblua5.1-0-dev
 
 git clone https://github.com/vim/vim.git
 
+cd vim
+
 ./configure --with-features=huge \
               --enable-multibyte \
               --enable-rubyinterp \
@@ -18,8 +20,13 @@ git clone https://github.com/vim/vim.git
               --with-python-config-dir=/usr/lib/python2.7/config \
               --enable-perlinterp \
               --enable-luainterp \
-              --enable-gui=gtk2 --enable-cscope --prefix=/usr
-
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
+              --enable-gui=gtk2 --enable-cscope --prefix=/usr && \
+make VIMRUNTIMEDIR=/usr/share/vim/vim74 && \
 sudo make install
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
+
 
