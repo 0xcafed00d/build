a3278fc516fb34968ba7f44a9c546e1c4b41fcf2
Lee Witek <neomech@gmail.com>
2015-09-14 22:49:05 +0100

    added fish shell build

---
+++ build_fish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install git gcc g++ make autoconf pkg-config cmake
+sudo apt-get -y install build-essential ncurses-dev libncurses5-dev gettext
+
+git clone https://github.com/fish-shell/fish-shell.git
+
+cd fish-shell
+
+autoconf && \
+./configure && \
+make && \
+sudo make install
+
