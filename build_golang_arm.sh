4f0277a3f82a48d17d995e648cc2c74cc7f0e4ab
Lee Witek <neomech@gmail.com>
2015-09-19 23:17:34 +0100

    added go build on arm

---
+++ build_golang_arm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $HOME
+
+curl http://dave.cheney.net/paste/go-linux-arm-bootstrap-c788a8e.tbz | tar xj
+curl https://storage.googleapis.com/golang/go1.5.1.src.tar.gz | tar xz
+
+ulimit -s 1024
+
+cd $HOME/go/src
+
+env GO_TEST_TIMEOUT_SCALE=10 GOROOT_BOOTSTRAP=$HOME/go-linux-arm-bootstrap ./make.bash
+
+cd $HOME
+
+echo export PATH=\$PATH:\$HOME/go/bin >> .profile
+echo export GOPATH=\$HOME/development/go >> .profile
+echo export PATH=\$PATH:\$GOPATH/bin >> .profile
+
--- build_vim.sh
+++ build_vim.sh
@@ -27,9 +27,5 @@ cd vim
 make VIMRUNTIMEDIR=/usr/share/vim/vim74 && \
 sudo make install
 
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-sudo update-alternatives --set editor /usr/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-sudo update-alternatives --set vi /usr/bin/vim
 
 
